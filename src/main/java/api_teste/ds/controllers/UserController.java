a6d29a24bb952a5390edcc26f1486204dd7633b9
bryanchristsevero <bryancsevero@gmail.com>
2026-09-23 14:23:51 -0300

    Conclusão do UserController

--- src/main/java/api_teste/ds/controllers/UserController.java
+++ src/main/java/api_teste/ds/controllers/UserController.java
@@ -1,5 +1,6 @@
 package api_teste.ds.controllers;
 
+import api_teste.ds.DsApplication;
 //importa a classe URI para construir e manipular HTTP de novos recursos.
 import java.net.URI;
 //injeção automatica do Spring.
@@ -32,7 +33,7 @@ import api_teste.ds.models.User;
 //
 import api_teste.ds.models.User.CreateUser;
 //
-import api_teste.ds.models.User.updateUser;
+import api_teste.ds.models.User.UpdateUser;
 //
 import api_teste.ds.services.UserService;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,23 +48,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class UserController {
 
+    private final DsApplication dsApplication;
     @Autowired 
     private UserService userService;
+
+    UserController(DsApplication dsApplication) {
+        this.dsApplication = dsApplication;
+    }
 
     //mapeia requisições HTTP GET na rota "/user/{id}"
     @GetMapping ("/{id}")
     //método para buscar usuário por ID capturado da URL. 
     public ResponseEntity<User> findById(@PathVariable Long Id){
         User obj=this.userService.findById(Id); //invoca a busca do usuário através do ID recebido.
-        return ResponseEntity.ok().body(obj) //retorna código HTTP 200(ok) com o objeto User no corpo da resposta.
+        return ResponseEntity.ok().body(obj); //retorna código HTTP 200(ok) com o objeto User no corpo da resposta.
     } //fim do método findById.
 
+    //mapeia requisições HTTP POST na rota base "/user" (criação de novo usuário).
     @PostMapping
+    //valida regra de CreateUser e desserializa o corpo JSON.
     public ResponseEntity<Void> create(@Validated (CreateUser.class) @RequestBody User obj){
-        this.userService.create(obj);
-        URI url = ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{id}").buildAndExpand(obj.getId()).toUri();
-            return ResponseEntity.created(url).build();
+        this.userService.create(obj); //chama a camada de serviço para persistir o novo usuário no banco de dados.
+        URI url = ServletUriComponentsBuilder.fromCurrentRequest() //obtém a rota da requisição atual.
+            .path("/{id}").buildAndExpand(obj.getId()).toUri(); //adiciona o ID do usuário gerado no final do caminho da URI.
+            return ResponseEntity.created(url).build(); //retorna código HTTP 201(created) contendo a URL no cabeçalho location.
     }
-    
+
+    //mapeia requisições HTTP PUT na rota base "/user/{id}" (atualização do usuário).
+    @PutMapping("/{id}")
+    //aplica a regra de UpdateUser e recebe ID e JSON.
+    public ResponseEntity<Void> update(@Validated(UpdateUser.class)@RequestBody  User obj, @PathVariable Long id){
+        obj.setId(id); //garante que o ID do objeto a ser atualizado corresponde ao ID informado no parametro da URL.
+        this.userService.update(obj); //executa a atualização da senha do usuário no banco de dados.
+        return ResponseEntity.noContent().build(); //retorna código HTTP 204(No Content) indicando sucesso sem corpo de resposta.
+    }
+
+    //mapeia requisições HTTP DELETE na rota base "/user/{id}" (exclusão de usuário).
+    @DeleteMapping ("/{id}")
+    //captura o ID da URL a ser deletada.
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id); //invoca o método de deleção do serviço.
+        return ResponseEntity.noContent().build(); //retorna código HTTP 204 (No Content) confirmando a exclusão.
+    } 
 }
